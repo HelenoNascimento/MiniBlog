50ea143c8cf2b6143590ef0f0e9b297eaa14f019
Heleno Nascimento <heleno_55@hotmail.com>
2022-07-27 11:57:10 -0300

    criado novas paginas e tela de registro

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firebase";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtXiIxGJ2iR_q1dOtjIuoteB2SayjFr3U",
+  authDomain: "mini-blog-65f4e.firebaseapp.com",
+  projectId: "mini-blog-65f4e",
+  storageBucket: "mini-blog-65f4e.appspot.com",
+  messagingSenderId: "558170018214",
+  appId: "1:558170018214:web:a393679ff91c3bb7ffff67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const dv = getFirestore(app);
+
+export {db} 
