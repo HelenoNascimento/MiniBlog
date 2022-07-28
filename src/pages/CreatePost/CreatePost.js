ab31e2dfec02b545da0c662962ce1bf219f5f47b
Heleno Nascimento <heleno_55@hotmail.com>
2022-07-28 10:18:51 -0300

    funcionalidade para criação de post finalizada

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -3,8 +3,9 @@
 import styles from './CreatePost.module.css';
 
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext';
+import { useInsertDocument } from '../../hooks/useInsertDocument';
 
 const CreatePost = () => {
 const [ title, setTitle] = useState("");
@@ -13,8 +14,43 @@ const [ body, setBody] = useState("");
 const [  tags, setTags] = useState("");
 const [ formError, setFormError] = useState("");
 
+const { insertDocument, response} = useInsertDocument("posts");
+const{ user } = useAuthValue();
+const navigate= useNavigate();
+
 const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
+
+    
+    //validade image URL
+    try {
+        new URL(image)
+    } catch (error) {
+        setFormError("A imagem precisa ser uma URL")
+    }
+
+    //criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    //checar todos os valores
+    if(!title || !image || !body){
+        setFormError("Por favor, preencha todos os campos")
+    }
+
+    if(formError) return;
+
+    insertDocument({
+        title,
+        image,
+        body,
+        tags,
+        uid: user.uid,
+        createdBy: user.displayName
+    })
+
+    //redirect to home page
+    navigate("/");
 };
 
   return (
@@ -64,11 +100,12 @@ const handleSubmit = (e) => {
                 value={tags}
                 />
             </label>
-            <button className="btn">Cadastrar </button>
-            {/*
-            {!loading && <button className="btn">Cadastrar </button>}
-          {loading && <button className="btn" disabled>Aguarde... </button>}
-  {error && <p className="error">{error}</p>}*/}
+           
+           
+            {!response.loading && <button className="btn">Cadastrar </button>}
+            {response.loading && <button className="btn" disabled>Aguarde... </button>}
+            {response.error && <p className="error">{response.error}</p>}
+            {formError&& <p className="error">{formError}</p>}
         </form>
     </div>
   )
